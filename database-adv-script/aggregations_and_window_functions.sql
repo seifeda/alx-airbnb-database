2149ef83cdda149b3545c713222832a33ba39640
Seifeda <seifebekele07@gmail.com>
2025-11-01 11:06:57 +0300

    adding Rank and Row_Number on aggregation script

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -27,3 +27,18 @@ FROM Property p
     LEFT JOIN Booking b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name
 ORDER BY booking_rank;
+
+-- Rank properties based on the total number of bookings they have received
+SELECT
+    p.property_id,
+    p.property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM
+    properties p
+    LEFT JOIN
+    bookings b ON p.property_id = b.property_id
+GROUP BY
+    p.property_id, p.property_name
+ORDER BY
+    booking_rank;
